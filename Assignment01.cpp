6ff51a454bd9fab80ecffe1c34fc65c025528478
Tharaka Wijekularatne <tharusnow@gmail.com>
2022-01-26 22:53:57 -0800

    added code in main for testing

--- Assignment01.cpp
+++ Assignment01.cpp
@@ -70,18 +70,20 @@ template <typename type>
 const typename LL<type>::iterator& LL<type>::iterator::operator++(int) 
 {
     current = current->next;
+    return *this;
 }
 
 template <typename type>
 const typename LL<type>::iterator& LL<type>::iterator::operator--(int)
 {
     current = current->prev;
+    return *this;
 }
 
 template <typename type>
 bool LL<type>::iterator::operator==(const iterator& rhs) const 
 {
-    if(this == rhs) 
+    if(this->current == rhs.current) 
     {
         return true;
     }
@@ -91,7 +93,7 @@ bool LL<type>::iterator::operator==(const iterator& rhs) const
 template <typename type>
 bool LL<type>::iterator::operator!=(const iterator& rhs) const
 {
-    if(this != rhs) 
+    if(this->current != rhs.current) 
     {
         return true;
     }
@@ -116,7 +118,7 @@ LL<type>::LL(const LL<type>& copy)
 template <typename type>
 const LL<type>& LL<type>::operator=(const LL<type>& rhs)
 {
-    
+ 
     return *this;
 }
 
@@ -129,19 +131,19 @@ LL<type>::~LL()
 template <typename type>
 void LL<type>::tailInsert(const type& item)
 {
-
+    
 }
 
 template <typename type>
 typename LL<type>::iterator LL<type>::begin() const
 {
-
+    return iterator(head);
 }
 
 template <typename type>
 typename LL<type>::iterator LL<type>::end() const
 {
-
+    return iterator(tail);
 }
 
 template <typename type>
@@ -153,27 +155,41 @@ void LL<type>::swapNodes(iterator& it1, iterator& it2)
 //Main
 int main() 
 {
-    
     string filename;
     ifstream infile; 
     LL<int> list; 
     LL<int>::iterator it; 
     LL<int>::iterator nil(NULL);
-
+    int *i; 
     int temp;
 
-    cout << "Please enter the name of the imput file: " << endl;
+    cout << "Enter file with list: " << endl;
     cin >> filename;
 
     infile.open(filename); //open file
 
+    //i = NULL;
+    //while (i != List.begin()) 
+    // {
+
+    // }
 
     // for(int i = 0; i < 10; i++) {
     //     infile >> temp;
     //     cout << temp << endl;
     // }
 
+
+
+// list.tailInsert(1); list.tailInsert(2); list.tailInsert(3); list.tailInsert(4); list.tailInsert(5);
+// it = list.begin();
+// while (it != nil) {
+// std::cout << *it << endl;
+// it ++; }
+
     infile.close(); //close file
 
     return 0;
 }
+
+
